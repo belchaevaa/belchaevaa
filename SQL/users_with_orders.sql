21ee62aff87d34d0e7f545734b6bbb61bf4bba99
Пользователь <belchaevaa@yandex.ru>
2025-09-04 13:10:13 +0300

    redact

--- SQL/users_with_orders.sql
+++ SQL/users_with_orders.sql
@@ -1,4 +1,4 @@
--- Задача: Найти всех пользователей, которые сделали хотя бы 1 заказ
+Задача: Найти всех пользователей, которые сделали хотя бы 1 заказ
 SELECT users.customer_id, users.company_name, COUNT(orders.order_id) AS orders_count
 FROM users
 JOIN orders ON users.customer_id = orders.customer_id
